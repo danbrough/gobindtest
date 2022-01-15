5e4e9a565248334259d7517e948fac7a966f6d9b
Dan Brough <dan@danbrough.org>
2022-01-16 09:53:38 +1300

    update

--- scripts/build_jvm.sh
+++ scripts/build_jvm.sh
@@ -4,26 +4,13 @@ cd $(dirname $0) && cd ..
 source scripts/common.sh
 
 export SRCDIR=`pwd`
-
-build_env(){
-  export GOOS=linux
-  export GOARCH=$1
-  unset CC CXX
-  export CGO_ENABLED=1
-  #export GOPATH=$WORK
-  export CGO_LDFLAGS="-ldl -lpthread -fPIC"
-  export CGO_CFLAGS="-I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux -fPIC"
-}
-
-
-build(){
-  go build -tags openssl -v -x -ldflags -w -work -buildmode=c-shared \
-  -o=$SRCDIR/jvm/libs/$1/libkipfs.so ./test1
-}
-
-build_env $ARCH
-build $ARCH
-
-
+export GOOS=linux
+export GOARCH=$ARCH
+unset CC CXX
+export CGO_ENABLED=1
+export CGO_LDFLAGS="-ldl -lpthread -fPIC"
+export CGO_CFLAGS="-I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux -fPIC"
+go build -tags openssl  -v -ldflags -w -buildmode=c-shared \
+  -o=$SRCDIR/jvm/libs/$ARCH/libkipfs.so ./test1
 
 
