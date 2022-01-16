aae59b9f12f7ef201f2ff7ade1c3dc1ad5fb6702
Dan Brough <dan@danbrough.org>
2022-01-17 07:25:27 +1300

    update

--- jvm/build.gradle.kts
+++ jvm/build.gradle.kts
@@ -1,9 +1,57 @@
 plugins {
-  `java-library`
   kotlin("jvm")
+  `maven-publish`
 }
 
+group = ProjectVersions.GROUP_ID
+version = ProjectVersions.VERSION_NAME
+
 java {
-  sourceCompatibility = JavaVersion.VERSION_1_8
-  targetCompatibility = JavaVersion.VERSION_1_8
+  sourceCompatibility = ProjectVersions.JAVA_VERSION
+  targetCompatibility = ProjectVersions.JAVA_VERSION
+
+  withSourcesJar()
 }
+
+dependencies {
+  implementation(AndroidUtils.logging)
+}
+
+
+val buildLinuxAmd64 by tasks.registering(Jar::class) {
+  archiveFileName.set("linuxAmd64.jar")
+  from(file("libs/amd64"))
+}
+
+val buildLinuxArm64 by tasks.registering(Jar::class) {
+  archiveFileName.set("linuxArm64.jar")
+  from(file("libs/arm64"))
+}
+
+val buildWin32 by tasks.registering(Jar::class) {
+  archiveFileName.set("win32.jar")
+  from(layout.projectDirectory.dir("libs/win32"))
+}
+
+
+
+publishing {
+  publications {
+    create<MavenPublication>("default") {
+      from(components["java"])
+    }
+
+    listOf("win32", "linuxAmd64", "linuxArm64").forEach { arch ->
+      create<MavenPublication>(arch) {
+        artifactId = arch
+        artifact(tasks.getAt("build${arch.capitalize()}"))
+      }
+    }
+  }
+
+  repositories {
+    maven(ProjectVersions.MAVEN_REPO)
+  }
+
+}
+
