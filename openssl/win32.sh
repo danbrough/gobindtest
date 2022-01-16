a0a5fa9653458aba148f3e71de980f2abbe18d5e
Dan Brough <dan@danbrough.org>
2022-01-16 16:16:47 +1300

    update

--- openssl/win32.sh
+++ openssl/win32.sh
@@ -5,11 +5,16 @@ cd `dirname $0`
 source common.sh
 
 export OPENSSL_LIBS=$SCRIPTDIR/libs/win32
-cd $SRC
+export CC=x86_64-w64-mingw32-gcc
+export CXX=x86_64-w64-mingw32-cpp
+export WINDRES=x86_64-w64-mingw32-windres
+
+
 
 if [ -d $OPENSSL_LIBS ]; then
     echo not building openssl as $OPENSSL_LIBS exists
 else
+	cd $SRC
   ./Configure --prefix="$OPENSSL_LIBS" mingw64 no-shared || exit 1
   make install_sw || exit 1
 fi
